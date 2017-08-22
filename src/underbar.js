1065c0958ae60a5e514417964f124199415b2e9c
Edward Plato <edplatomail@gmail.com>
2017-08-22 16:37:25 -0700

    Pass tests through reject

--- src/underbar.js
+++ src/underbar.js
@@ -82,12 +82,22 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var results = [];
+    _.each(collection, function(value) {
+      if(test(value)) {
+        results.push(value);
+      }
+    });
+    return results;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+      return _.filter(collection, function(value){
+        return !test(value);
+      });
   };
 
   // Produce a duplicate-free version of the array.
